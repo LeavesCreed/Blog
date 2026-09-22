4521a80d7923a6a028814b0e49142161483a9675
leavescreed <leavescreed@qq.com>
2026-09-22 13:39:29 +0800

    feat: 更新LightDarkSwitch组件以支持Astro客户端指令属性，优化archive页面以获取分类和标签列表

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 
 // 允许接收 Astro 客户端指令属性
-  interface $$Props {
-    [key: string]: any;
-  }
+let props = $props();
 
 import { AUTO_MODE, DARK_MODE, LIGHT_MODE } from "@constants/constants.ts";
 import I18nKey from "@i18n/i18nKey";

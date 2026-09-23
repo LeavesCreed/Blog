d56fdd35e1ec6320a908f1883f70fb7a86817008
leavescreed <leavescreed@qq.com>
2026-09-23 14:40:07 +0800

    feat: 更新站点标题和副标题，添加关于AUR投毒事件的文章

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "HOME",
-	subtitle: "只是记录",
+	title: "LeacesCreed的抽屉",
+	subtitle: "放了些杂物",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 10, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
---
+++ src/content/posts/AUR投毒事件.md
@@ -0,0 +1,14 @@
+---
+title: 关于AUR投毒事件
+published: 2026-09-22
+description: ''
+image: ''
+tags: []
+category: ''
+draft: true 
+lang: ''
+---
+
+灵感来源于今年6.17写的日记。
+
+那时候因为接近放假，就换系统去玩一玩。我的双系统是Windows+ArchLinux。众所周知，ArchLinux是滚动更新的，当时距离我上一次系统和软件更新已经过了半个多月，所以用pacman更新系统和软件包是在所难免的。又众所周知，官方仓库里没有QQ的包，想要使用QQ(微信也是同理)必须去AUR下载社区版本
